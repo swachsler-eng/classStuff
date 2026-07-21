15009d1136dbcb38edee4126f2cb9ff70ac4dca9
swachsler-eng <swachsler@outlook.com>
2026-07-21 11:03:33 -0400

    new

---
+++ 58/one.js
@@ -0,0 +1,6 @@
+console.log('Hello World')
+// alert("hello from js");
+const state = prompt("how are u");
+console.log("you are feeling", state);
+// alert("you are feeling" + state);
+alert(`You are feeling ${state}`);
